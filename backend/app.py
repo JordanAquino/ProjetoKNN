0e78aa8ac1367a5535086b6d0501de5362515ddc
Jordan Aquino <84852943+JordanAquino@users.noreply.github.com>
2024-10-28 01:53:23 -0300

    Update

--- backend/app.py
+++ backend/app.py
@@ -108,9 +108,6 @@ def autocomplete():
     
     return jsonify(suggestions)
 
-def open_browser():
-    webbrowser.open("http://127.0.0.1:5000/")
-
 if __name__ == '__main__':
-    threading.Timer(1, open_browser).start()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
